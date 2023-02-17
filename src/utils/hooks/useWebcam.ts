8c6ac1d552b18d9ae0b72d42aaccd371ac1f7104
Soonho Kwon <percy3368@gmail.com>
2023-02-18 02:21:58 +0900

    Fix IOS

--- src/utils/hooks/useWebcam.ts
+++ src/utils/hooks/useWebcam.ts
@@ -13,19 +13,19 @@ export const useWebcam = (size?: number) => {
       const _webcam = new Webcam(size, size, true);
       await _webcam.setup();
       
-      setMsg(webcamRef.current?.tagName || "ERROR")
-      webcamRef.current?.appendChild(_webcam.canvas);
-      if (isIOS()) {
-        const webCamVideo = document.getElementsByTagName("video")[0];
-        try {
-          webCamVideo.setAttribute("playsinline", 'playsinline');
-          webCamVideo.muted = true;
-          webCamVideo.style.width = `${size}px`;
-          webCamVideo.style.height = `${size}px`;
-        } catch (e) {
-          setMsg(msg => msg + (e as Error).message)
-        }
+      setMsg(webcamRef.current?.tagName || "ERROR");
+      webcamRef.current?.appendChild(_webcam.webcam);
+      // if (isIOS()) {
+      const webCamVideo = document.getElementsByTagName("video")[0];
+      try {
+        webCamVideo.setAttribute("playsinline", "playsinline");
+        webCamVideo.muted = true;
+        webCamVideo.style.width = `${size}px`;
+        webCamVideo.style.height = `${size}px`;
+      } catch (e) {
+        setMsg(msg => msg + (e as Error).message);
       }
+      // }
       
       await _webcam.play();
       return _webcam;
