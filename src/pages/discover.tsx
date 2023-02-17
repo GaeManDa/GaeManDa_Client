b01d39f1169c0a81d5234b224c89653e26449d32
Soonho Kwon <percy3368@gmail.com>
2023-02-18 08:36:00 +0900

    qwer

--- src/pages/discover.tsx
+++ src/pages/discover.tsx
@@ -3,32 +3,33 @@ import { useWebcam } from "../utils/hooks/useWebcam";
 import { useEffect, useState } from "react";
 import { Profile } from "../components/discover/Profile";
 import { useQuery } from "@tanstack/react-query";
-import { getAllDogList, getLikedDogList } from "../api/fetchers";
+import { getLikedDogList } from "../api/fetchers";
 import UserInfoState from "../atoms/UserInfoAtom";
 import { useRecoilValue } from "recoil";
+import { useRouter } from "next/router";
 
 export default function Discover() {
   const { webcam, webcamRef } = useWebcam();
   const { rate, resetRate, start } = usePrediction(webcam);
-  
+  const { push } = useRouter();
   const user = useRecoilValue(UserInfoState);
   
   const { data: dogs } = useQuery({ queryKey: ["dogs"], queryFn: () => getLikedDogList(user.id) });
   
   const [idx, setIdx] = useState(0);
-
+  
   const [transition, setTransition] = useState(false);
   
-  const next = () => {
-    setTransition(true);
-  };
   
   useEffect(() => {
     if (rate > 20 || rate < -20) {
-      next();
+      setTransition(true);
       resetRate();
+      if (dogs?.length && idx + 1 >= dogs.length) {
+        push("/main");
+      }
     }
-  }, [rate, resetRate]);
+  }, [rate, resetRate, setTransition, dogs?.length, idx, push]);
   
   const loadNext = () => {setIdx(idx => idx + 1);};
   
@@ -36,7 +37,6 @@ export default function Discover() {
     setTransition(false);
     start();
   };
-  
   
   if (!dogs) return (<div>Loading</div>);
   
@@ -55,13 +55,6 @@ export default function Discover() {
         className="z-0"
         rate={0}
       />
-      
-      
-      <div className="absolute bottom-0 left-0 z-50">
-        qwer
-        <button className="z-40" onClick={next}>next</button>
-      </div>
-      
       
       <div
         className="absolute top-2 right-2 rounded-lg overflow-hidden drop-shadow-xl z-20
